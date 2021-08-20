20fe213e251a67b0ee298c21bde6c85b65c2a62b
Priscila Duarte <duartecostap@gmail.com>
2021-08-20 16:31:48 -0300

    Sobe introdução

---
+++ hello-typescript/main.ts
@@ -0,0 +1,23 @@
+var minhaVar = 'minha variavel';
+
+function minhaFunc(x, y) {
+    return x + y;
+}
+
+let num = 2;
+const PI = 3.14;
+
+var numeros = [1, 2, 3];
+numeros.map(function(){
+    return valor * 2;
+});
+numeros.map( valor => valor * 2);
+
+class Matematica {
+    soma(x, y){
+        return x + y;
+    }
+}
+
+var n1: any = 'asdfsad';
+n1 = 4;

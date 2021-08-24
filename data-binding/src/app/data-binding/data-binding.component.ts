c3b0e8ae30935dcc9ad3fad18d5d9fa5a1a4c2fb
Priscila Duarte <duartecostap@gmail.com>
2021-08-23 22:15:12 -0300

    Interpolação

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,28 @@
+import { ParseTreeResult } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url = 'http://loiane.com';
+  cursoAngular = true;
+  urlImagem = 'http://lorempixel.com/400/200/nature/';
+
+  getValor(){
+    return 1;
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
